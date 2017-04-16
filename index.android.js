297e6960d9585b3b2ad4c9c725909fa00abe9a8c
Yusuf Emre Durur <ydurur@ebay.com>
2017-04-16 16:46:38 +0300

    ggDictionary name fixed

--- index.android.js
+++ index.android.js
@@ -3,4 +3,4 @@ import { AppRegistry }  from 'react-native';
 
 import  App from './src/app';
 
-AppRegistry.registerComponent('ggDictonary', () => App);
+AppRegistry.registerComponent('ggDictionary', () => App);
